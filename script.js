c5528438dce292d4e0b8928d63724bbaeaf76ce6
Windisley <windisleyl@gmail.com>
2024-10-31 00:19:31 -0300

    Agora o cliente consegue definir o tamanho da senha

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const $textarea = document.querySelector("#generetes-password")
 const buttonAdd = document.querySelector(".addpassword")
 const buttonClear = document.querySelector(".clear")
+const lenghtInput = document.querySelector("#lenghtpassword")
 
 const text = ["A","B","C","D","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","W","U","Y","Z",
                 "a","b","c","d","f","g","h","i","j","k","l","m","n","o","p","q","r","s","w","y","z",
@@ -11,17 +12,25 @@ buttonAdd.addEventListener("click", ()=>{
                     
     const content = []
     const textZ = text.length
+    const inputValue = lenghtInput.value
+        
+        
+    if(inputValue !== ""){
+        for(let i =0; i <  inputValue; i++){
+                const indextext = Math.floor(Math.random() * textZ)
+                
+                content.push(text[indextext])
+              
+                
+            }
+
+    }else{
+        alert("Defina O Tamanho Da Sua Senha")
+    }
     
         
-        for(let i =0; i < 14; i++){
-            const indextext = Math.floor(Math.random() * textZ)
-           
-            content.push(text[indextext])
-          
-            
-        }
-        
         if(content.length > 0){
+           
             $textarea.value = ""
           
             $textarea.value += content.join("")
