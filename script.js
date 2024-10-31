e5d4e2373846b05edb5417bf1619c0e49242d0df
Windisley <windisleyl@gmail.com>
2024-10-30 23:51:32 -0300

    Gerador de Senhas

---
+++ script.js
@@ -0,0 +1,42 @@
+const $textarea = document.querySelector("#generetes-password")
+const buttonAdd = document.querySelector(".addpassword")
+const buttonClear = document.querySelector(".clear")
+
+const text = ["A","B","C","D","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","W","U","Y","Z",
+                "a","b","c","d","f","g","h","i","j","k","l","m","n","o","p","q","r","s","w","y","z",
+                "!","@","#","$","%","&","*",":","?"]
+    
+            
+buttonAdd.addEventListener("click", ()=>{
+                    
+    const content = []
+    const textZ = text.length
+    
+        
+        for(let i =0; i < 14; i++){
+            const indextext = Math.floor(Math.random() * textZ)
+           
+            content.push(text[indextext])
+          
+            
+        }
+        
+        if(content.length > 0){
+            $textarea.value = ""
+           content.map((item)=>{
+            $textarea.value += item
+           })
+        }
+
+        
+    })
+    
+    buttonClear.addEventListener("click", ()=>{
+        if($textarea.value != ""){
+            $textarea.value = ""
+
+        }else{
+            alert("Campo Vazio, Gere uma Senha Primeiro!")
+        }
+    })
+    
